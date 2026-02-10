d77d5eff95ed0e1103b7a6cbba8ed8e77d32dc6c
Rustam Ganeyev <rganeyev@gmail.com>
2026-02-10 14:42:52 +0000

    Add 'learn' icon to ICONS and AVAILABLE_ICONS

--- src/components/IconLibrary.ts
+++ src/components/IconLibrary.ts
@@ -2,7 +2,8 @@ export const ICONS: { [key: string]: any } = {
   default: require('../../assets/images/default-icon.png'),
   finance: require('../../assets/images/icons/icon_finance.png'),
   game: require('../../assets/images/icons/icon_game.png'),
+  learn: require('../../assets/images/icons/icon_learn.png'),
   // Add other icons here following the same pattern
 };
 
-export const AVAILABLE_ICONS: string[] = ['finance', 'game'];
+export const AVAILABLE_ICONS: string[] = ['finance', 'game', 'learn'];
